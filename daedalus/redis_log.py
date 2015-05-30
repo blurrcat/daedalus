cad610532b53c22f4368fba4e4bb969e37f81101
blurrcat <blurrcat@gmail.com>
2015-05-30 18:05:45 +0800

    add tests for redis logger

--- daedalus/redis_log.py
+++ daedalus/redis_log.py
@@ -4,26 +4,29 @@ from redis import StrictRedis
 
 
 class RedisLog(object):
-    PREFIX = 'log'
 
-    def __init__(self, redis_url, ttl=3600):
+    def __init__(self, redis_url='redis://127.0.0.1', ttl=3600, prefix='log'):
         self.ttl = ttl
-        self.c = StrictRedis.from_url(redis_url)
+        self.prefix = prefix
+        self.redis = StrictRedis.from_url(redis_url)
 
     def _key(self, jid):
-        return '{}.{}'.format(self.PREFIX, jid)
+        return '{}.{}'.format(self.prefix, jid)
 
     def touch(self, jid):
         k = self._key(jid)
-        self.c.lpush(jid, k, '')
-        self.c.expire(k, self.ttl)
+        self.redis.lpush(k, '')
+        self.redis.expire(k, self.ttl)
 
     def append(self, jid, content):
         """
         returns length of log after append
         """
         if content:
-            return self.c.rpush(self._key(jid), content)
+            if isinstance(content, str):
+                content = content.encode('utf-8')
+            return self.redis.rpush(self._key(jid), content)
 
     def tail(self, jid, since=1):
-        return self.c.lrange(self._key(jid), since, -1)
+        return [item.decode('utf-8')
+                for item in self.redis.lrange(self._key(jid), since, -1)]
